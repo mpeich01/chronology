5931f54e76feff26554017e8cc88b3b69666db01
bgivner <bgivner@umich.edu>
2020-05-25 23:21:06 -0400

    axis class

--- canvas.js
+++ canvas.js
@@ -5,6 +5,8 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 var c = canvas.getContext('2d');
+var slope = .15
+var lineWidth = 2;
 
 // Class for the wave representing a nation's trajectory
 function Wave(name, color, trajectory, speed){
@@ -13,47 +15,95 @@ function Wave(name, color, trajectory, speed){
     this.color  = color
     this.traj   = trajectory
     this.speed  = speed
+    
     this.draw = function () {
         c.beginPath()
-c.moveTo(0, canvas.height)
-c.linewidth = 10
-var startWave = (2 * canvas.height / 3)
+        c.moveTo(0, canvas.height)
+        c.linewidth = 10
+        var startWave = (2 * canvas.height / 3)
 
-for(let i = 0; i < canvas.width; i++){ 
-    c.lineTo(i, (-slope * i) + startWave + 100 * Math.sin(.01 * i))
+        for(let i = 0; i < canvas.width; i++){ 
+            c.lineTo(i, (-slope * i) + startWave + 100 * Math.sin(.01 * i))
+        }
+    }        
 }
 
+function XZaxis(slope, isZ){ //.15
+
+    this.slope = slope
+    var movTo = 2/3
+    var linTo = -1 * slope
+    if(isZ){
+        movTo = .5
+        linTo = 1.8 * slope 
     }
-    
-        
+
+    this.draw = function() { 
+        c.beginPath()
+        c.moveTo(0,  movTo * canvas.height)
+        c.lineWidth = 1
+        c.lineTo(canvas.width, ((linTo) * canvas.width) + (2 * canvas.height / 3))
+        c.strokeStyle = 'red'
+        c.stroke();
+    }
 }
 
-var slope = 0.15
-var axisWidth = 2
-//x axis
-c.beginPath()
-c.moveTo(0, 2 * canvas.height / 3)
-c.lineWidth = 1
-c.lineTo(canvas.width, (- canvas.width * slope) + (2 * canvas.height / 3))
-c.stroke();
+    xAxis = new XZaxis(slope, false);
+    xAxis.draw();
+    zAxis = new XZaxis(slope, true);
+    zAxis.draw()
+//xaxis
+    // this.draw = function(){   
+    //     c.beginPath()
+    //     c.moveTo(0, (2/3) * canvas.height)
+    //     c.lineWidth = 1
+    //     c.lineTo(canvas.width, ((-1 * slope) * canvas.width) + (2 * canvas.height / 3))
+    //     c.strokeStyle = 'red'
+    //     c.stroke();
+    // }
+
+
+// function Zaxis(slope){ // slope = .15
+//     this.draw = function(){
+//         c.moveTo(0, canvas.height / 2)
+//         c.linewidth = lineWidth
+//         c.lineTo(canvas.width, (canvas.width * 1.8 * slope) + (2 * canvas.height / 3))
+//         c.strokeStyle = 'red'
+//         c.stroke();     
+//     }
+// }
+
+
+// //z-axis
+// c.moveTo(0, canvas.height / 2)
+// c.linewidth = lineWidth
+// c.lineTo(canvas.width, (canvas.width * 1.8 * slope) + (2 * canvas.height / 3))
+// c.strokeStyle = 'red'
+// c.stroke();
 
 // //bottom axis
-// c.moveTo(0, 5 * canvas.height / 6)
-// c.linewidth = 10
-// c.lineTo(canvas.width, (- canvas.width * slope) + (5 * canvas.height / 6))
-// c.stroke();
+c.beginPath()
+c.moveTo(0, (2 * (canvas.height / 3)) - 120)
+c.lineWidth = 1
+c.lineTo(canvas.width, (- canvas.width * slope) + ((2 * canvas.height / 3) - 120))
+c.strokeStyle = 'red'
+c.stroke();
+
+//Top Axis
+c.beginPath()
+c.moveTo(0, (2 * (canvas.height / 3)) + 120)
+c.lineWidth = 1
+c.lineTo(canvas.width, (- canvas.width * slope) + ((2 * canvas.height / 3) + 120))
+c.strokeStyle = 'red'
+c.stroke();
 
 //y-axis
 c.moveTo(271, canvas.height)
-c.lineWidth = axisWidth
+c.lineWidth = 1
 c.lineTo(271, 0)
+c.strokeStyle = 'red'
 c.stroke();
 
-//z-axis
-c.moveTo(0, canvas.height / 2)
-c.linewidth = axisWidth
-c.lineTo(canvas.width, (canvas.width * 3.75 * slope) + (2 * canvas.height / 3))
-c.stroke();
 
 //sine 
 c.beginPath()
@@ -62,11 +112,27 @@ c.lineWidth = 10
 c.color
 var startWave = (7 * canvas.height / 10)
 
-for(let i = 0; i < canvas.width; i++){ 
-    c.lineTo(i, (-slope * i) + startWave + 100 * Math.sin(.01 * i))
-}
+for(let i = 1; i < canvas.width; i++){ 
+    //Sine Wave = slope W.R.T. x-position(height increases left -> right)
+    //          + Starting height
+    //          + amplitude / cubed root of x-position(decreasing amplitude left -> right)
+    //          * sine function of (horizontal shrink of cons)
+    //            reference:  
+    //              c.lineTo(i, (-slope * i) + startWave + (500 / (Math.cbrt(i))) * Math.sin(.02 * i))
 
+    c.lineTo(i, (-slope * i) + startWave + (500 / (Math.cbrt(i))) * Math.sin(.02 * i))
+}
+c.strokeStyle = 'purple'
 c.stroke()
+
+//cWheel
+c.beginPath()
+c.ellipse(300, 450, 80, 120, (Math.PI), 0, 2 * Math.PI, false)
+c.lineWidth = 10
+c.strokeStyle = 'black'
+c.stroke()
+c.fillStyle = 'yellow'
+c.fill()
 
 
 
