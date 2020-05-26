4e8f9885f89379e4a0bda14f1492f3ea99725b65
bgivner <bgivner@umich.edu>
2020-05-26 14:17:44 -0400

    animations

--- canvas.js
+++ canvas.js
@@ -1,10 +1,13 @@
-var canvas = document.querySelector('Canvas');
-console.log(canvas);
+var layer1 = document.getElementById("layer1");
+var layer2 = document.getElementById("layer2");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const  width = 1302
+const  height = 744;
 
-var c = canvas.getContext('2d');
+layer1.width = width; //Givners screen is 1302 x 744
+layer1.height = height;
+
+var c = layer1.getContext('2d');
 var slope = 0.15;
 var lineWidth = 2;
 
@@ -15,15 +18,15 @@ function Wave(name, color, slope, amp) {
 
 	this.slope = slope; // slope for 3d perspective
 	this.amp = amp; // amplitude
-	this.startWave = 7 * canvas.height / 10; // starting height of the wave
+	this.startWave = 7 * layer1.height / 10; // starting height of the wave
 	this.wavelength = -0.02;
 	this.frequency = 0.01; //incrementer in animation
 
 	// this.draw = function() {
 	// 	c.beginPath();
-	// 	c.moveTo(0, canvas.height);
+	// 	c.moveTo(0, layer1.height);
 	// 	c.lineWidth = 10;
-	// 	for (let i = 0; i < canvas.width; i++) {
+	// 	for (let i = 0; i < layer1.width; i++) {
 	// 		//Sine Wave = slope W.R.T. x-position(height increases left -> right)
 	// 		//          + Starting height
 	// 		//          + amplitude / cubed root of x-position(decreasing amplitude left -> right)
@@ -41,7 +44,6 @@ function Wave(name, color, slope, amp) {
 
 function XZaxis(slope, isZ) {
 	//slope is currently .15
-
 	this.slope = slope;
 	var movTo = 2 / 3;
 	var linTo = -1 * slope;
@@ -52,66 +54,91 @@ function XZaxis(slope, isZ) {
 
 	this.draw = function() {
 		c.beginPath();
-		c.moveTo(0, movTo * canvas.height);
+		c.moveTo(0, movTo * layer1.height);
 		c.lineWidth = 1;
-		c.lineTo(canvas.width, linTo * canvas.width + 2 * canvas.height / 3);
+		c.lineTo(layer1.width, linTo * layer1.width + 2 * layer1.height / 3);
 		c.strokeStyle = 'red';
 		c.stroke();
 	};
+	this.updateZ = function(){
+		this.draw();
+		movTo += .0006;
+		linTo += .0006;
+	}
 }
-
-function Yaxis() {
-	c.moveTo(240, canvas.height);
-	c.lineWidth = 1;
-	c.lineTo(240, 0);
-	c.strokeStyle = 'red';
-	c.stroke();
+function Yaxis(x){ //240
+	this.x = x;
+	this.draw = function() {
+		c.moveTo(this.x, layer1.height);
+		c.lineWidth = 1;
+		c.lineTo(this.x, 0);
+		c.strokeStyle = 'red';
+		c.stroke();
+	}
+	this.update = function(){
+		this.draw()
+		this.x--
+	}
 }
+	
 
-function cWheel(x, y) {
+function cWheel(x, y, isLeft) {
 	this.x = x;
 	this.y = y;
 
 	this.draw = function() {
 		c.beginPath();
-		c.ellipse(this.x, this.y, 60, 120, Math.PI, 0, 2 * Math.PI, false);
-		c.lineWidth = 10;
-		c.strokeStyle = 'black';
-		c.stroke();
+		c.moveTo(this.x, this.y)
+		c.ellipse(this.x, this.y, 60, 120, Math.PI, Math.PI / 2, 3 * Math.PI / 2, isLeft)	
+		c.lineWidth = 100;
 		c.fillStyle = 'yellow';
 		c.fill();
 	};
 
 	this.update = function() {
+		c.moveTo(this.x, this.y)
 		dx = 0.1;
-		dy = 0.1118033;
-		c.ellipse(this.x, this.y, 60, 120, Math.PI, 0, 2 * Math.PI, false);
+		dy = 0.01118033;
+		c.ellipse(this.x, this.y, 60, 120, Math.PI, Math.PI / 2, 3 * Math.PI / 2, isLeft)
 		this.x += dx;
-		this.y += dy;
+		this.y -= dy;
+		if(this.x >= 1000){
+			this.x -= dx
+			this.y += dy
+		}
+		c.fillStyle = 'yellow';
+		c.fill();
 	};
 }
 
 sine = new Wave('boner', 'purple', slope, 500);
+
 xAxis = new XZaxis(slope, false);
 zAxis = new XZaxis(slope, true);
+yAxis = new Yaxis(240);
+
+leftcwheel = new cWheel(300, 450, true);
+rightcwheel = new cWheel(300, 450, false);
+ 
 
 c.strokeStyle = 'purple';
+
+zAxis.draw();
+
 function animate() {
 	requestAnimationFrame(animate);
-	c.fillStyle = 'rgba(0,0,0,0.01)';
-
-	c.fillRect(0, 0, canvas.width, canvas.height);
-
+	
+	
+	c.clearRect(0, 0, layer1.width, layer1.height);
 	xAxis.draw();
-	zAxis.draw();
-	Yaxis();
-	cWheel(300, 450, slope);
-
+	yAxis.update();
+	leftcwheel.update()
+	zAxis.updateZ();
 	c.beginPath();
-	c.moveTo(0, canvas.height / 2);
+	c.moveTo(0, layer1.height / 2);
 	c.lineWidth = 10;
 
-	for (let i = 1; i < canvas.width; i++) {
+	for (let i = 1; i < layer1.width; i++) {
 		//Sine Wave = slope W.R.T. x-position(height increases left -> right)
 		//          + Starting height
 		//          + amplitude / cubed root of x-position(decreasing amplitude left -> right)
@@ -119,14 +146,16 @@ function animate() {
 		//            reference:
 		//              c.lineTo(i, (-slope * i) + startWave + (500 / (Math.cbrt(i))) * Math.sin(.02 * i + .frequency))
 
-		c.lineTo(
-			i,
-			-slope * i + sine.startWave + sine.amp / Math.cbrt(i) * Math.sin(sine.wavelength * i + sine.frequency)
+		c.lineTo(i,-slope * i + sine.startWave + sine.amp / Math.cbrt(i) * Math.sin(sine.wavelength * i + sine.frequency)
 		);
 	}
 	c.strokeStyle = 'hsl(255, 50%, 50%)';
 	c.stroke();
+
+	c.beginPath();
+	rightcwheel.update()
 	sine.frequency += 0.01;
+
 }
 
 animate();
