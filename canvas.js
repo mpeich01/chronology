6dce11f363e9427a8b39768cd600c2089a0571c9
bgivner <bgivner@umich.edu>
2020-05-26 15:01:24 -0400

    animations2

--- canvas.js
+++ canvas.js
@@ -6,8 +6,12 @@ const  height = 744;
 
 layer1.width = width; //Givners screen is 1302 x 744
 layer1.height = height;
+layer2.width = width;
+layer2.height = height;
 
-var c = layer1.getContext('2d');
+var c1 = layer1.getContext('2d');
+var c2 = layer2.getContext('2d');
+
 var slope = 0.15;
 var lineWidth = 2;
 
@@ -23,20 +27,20 @@ function Wave(name, color, slope, amp) {
 	this.frequency = 0.01; //incrementer in animation
 
 	// this.draw = function() {
-	// 	c.beginPath();
-	// 	c.moveTo(0, layer1.height);
-	// 	c.lineWidth = 10;
+	// 	c1.beginPath();
+	// 	c1.moveTo(0, layer1.height);
+	// 	c1.lineWidth = 10;
 	// 	for (let i = 0; i < layer1.width; i++) {
 	// 		//Sine Wave = slope W.R.T. x-position(height increases left -> right)
 	// 		//          + Starting height
 	// 		//          + amplitude / cubed root of x-position(decreasing amplitude left -> right)
 	// 		//          * sine function of (horizontal shrink of cons + frequency)
 	// 		//            reference:
-	// 		//              c.lineTo(i, (-slope * i) + startWave + (500 / (Math.cbrt(i))) * Math.sin(.02 * i + .frequency))
+	// 		//              c1.lineTo(i, (-slope * i) + startWave + (500 / (Math.cbrt(i))) * Math.sin(.02 * i + .frequency))
 
-	// 		c.lineTo(i, -slope * i + this.startWave + amp / Math.cbrt(i) * Math.sin(wavelength * i));
-	// 		c.strokeStyle = color;
-	// 		c.stroke();
+	// 		c1.lineTo(i, -slope * i + this.startWave + amp / Math.cbrt(i) * Math.sin(wavelength * i));
+	// 		c1.strokeStyle = color;
+	// 		c1.stroke();
 	// 		this.frequency += this.frequency;
 	// 	}
 	// }
@@ -53,12 +57,12 @@ function XZaxis(slope, isZ) {
 	}
 
 	this.draw = function() {
-		c.beginPath();
-		c.moveTo(0, movTo * layer1.height);
-		c.lineWidth = 1;
-		c.lineTo(layer1.width, linTo * layer1.width + 2 * layer1.height / 3);
-		c.strokeStyle = 'red';
-		c.stroke();
+		c1.beginPath();
+		c1.moveTo(0, movTo * layer1.height);
+		c1.lineWidth = 1;
+		c1.lineTo(layer1.width, linTo * layer1.width + 2 * layer1.height / 3);
+		c1.strokeStyle = 'red';
+		c1.stroke();
 	};
 	this.updateZ = function(){
 		this.draw();
@@ -69,11 +73,11 @@ function XZaxis(slope, isZ) {
 function Yaxis(x){ //240
 	this.x = x;
 	this.draw = function() {
-		c.moveTo(this.x, layer1.height);
-		c.lineWidth = 1;
-		c.lineTo(this.x, 0);
-		c.strokeStyle = 'red';
-		c.stroke();
+		c1.moveTo(this.x, layer1.height);
+		c1.lineWidth = 1;
+		c1.lineTo(this.x, 0);
+		c1.strokeStyle = 'red';
+		c1.stroke();
 	}
 	this.update = function(){
 		this.draw()
@@ -87,31 +91,29 @@ function cWheel(x, y, isLeft) {
 	this.y = y;
 
 	this.draw = function() {
-		c.beginPath();
-		c.moveTo(this.x, this.y)
-		c.ellipse(this.x, this.y, 60, 120, Math.PI, Math.PI / 2, 3 * Math.PI / 2, isLeft)	
-		c.lineWidth = 100;
-		c.fillStyle = 'yellow';
-		c.fill();
+		c1.beginPath();
+		c1.moveTo(this.x, this.y)
+		c1.ellipse(this.x, this.y, 60, 120, Math.PI, Math.PI / 2, 3 * Math.PI / 2, isLeft)	
+		c1.lineWidth = 100;
+		c1.fillStyle = 'yellow';
+		c1.fill();
 	};
 
 	this.update = function() {
-		c.moveTo(this.x, this.y)
+		c1.moveTo(this.x, this.y)
 		dx = 0.1;
 		dy = 0.01118033;
-		c.ellipse(this.x, this.y, 60, 120, Math.PI, Math.PI / 2, 3 * Math.PI / 2, isLeft)
-		this.x += dx;
-		this.y -= dy;
-		if(this.x >= 1000){
-			this.x -= dx
-			this.y += dy
+		c1.ellipse(this.x, this.y, 60, 120, Math.PI, Math.PI / 2, 3 * Math.PI / 2, isLeft)
+		if(this.x < 1000) { //So it stops at 1000 and doesnt go off screen
+			this.x += dx;
+			this.y -= dy;
 		}
-		c.fillStyle = 'yellow';
-		c.fill();
+		c1.fillStyle = 'yellow';
+		c1.fill();
 	};
 }
 
-sine = new Wave('boner', 'purple', slope, 500);
+sine = new Wave('Israel', 'blue', slope, 500);
 
 xAxis = new XZaxis(slope, false);
 zAxis = new XZaxis(slope, true);
@@ -121,7 +123,7 @@ leftcwheel = new cWheel(300, 450, true);
 rightcwheel = new cWheel(300, 450, false);
  
 
-c.strokeStyle = 'purple';
+c1.strokeStyle = 'purple';
 
 zAxis.draw();
 
@@ -129,30 +131,36 @@ function animate() {
 	requestAnimationFrame(animate);
 	
 	
-	c.clearRect(0, 0, layer1.width, layer1.height);
+	c1.clearRect(0, 0, layer1.width, layer1.height);
+	//c2.fillStyle = 'rgba(0,0,0, 0.01)';
+	c2.clearRect(0, 0, layer1.width, layer1.height);
+
+	//axes (go on layer1)
 	xAxis.draw();
 	yAxis.update();
-	leftcwheel.update()
+	leftcwheel.update();
 	zAxis.updateZ();
-	c.beginPath();
-	c.moveTo(0, layer1.height / 2);
-	c.lineWidth = 10;
 
-	for (let i = 1; i < layer1.width; i++) {
+	// sine wave(goes on layer2)
+	c2.beginPath();
+	c2.moveTo(0, layer2.height / 2);
+	c2.lineWidth = 10;
+
+	for (let i = 1; i < layer2.width; i++) {
 		//Sine Wave = slope W.R.T. x-position(height increases left -> right)
 		//          + Starting height
 		//          + amplitude / cubed root of x-position(decreasing amplitude left -> right)
 		//          * sine function of (horizontal shrink of cons + frequency)
 		//            reference:
-		//              c.lineTo(i, (-slope * i) + startWave + (500 / (Math.cbrt(i))) * Math.sin(.02 * i + .frequency))
+		//              c1.lineTo(i, (-slope * i) + startWave + (500 / (Math.cbrt(i))) * Math.sin(.02 * i + .frequency))
 
-		c.lineTo(i,-slope * i + sine.startWave + sine.amp / Math.cbrt(i) * Math.sin(sine.wavelength * i + sine.frequency)
-		);
+		c2.lineTo(i,-slope * i + sine.startWave + sine.amp / Math.cbrt(i) * Math.sin(sine.wavelength * i + sine.frequency));
 	}
-	c.strokeStyle = 'hsl(255, 50%, 50%)';
-	c.stroke();
 
-	c.beginPath();
+	c2.strokeStyle = 'hsl(255, 50%, 50%)';
+	c2.stroke();
+
+	c2.beginPath();
 	rightcwheel.update()
 	sine.frequency += 0.01;
 
